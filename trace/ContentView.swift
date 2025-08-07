067a55c82a979ce47f48ed7e6b90df2aab3a9ae7
Arjun Komath <arjun@hey.com>
2025-08-07 14:57:59 +1000

    Initial Commit

---
+++ trace/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  trace
+//
+//  Created by Arjun on 7/8/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ trace/traceApp.swift
@@ -0,0 +1,17 @@
+//
+//  traceApp.swift
+//  trace
+//
+//  Created by Arjun on 7/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct traceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
